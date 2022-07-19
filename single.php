1958a99d7c5aa6bd84ed25977031f0e8b11d3cd9
Anthony <anthony.quiranza@upec.edu.ec>
2022-07-19 11:05:11 -0500

    first commit

---
+++ single.php
@@ -0,0 +1,17 @@
+<?php get_header() ?>
+
+<?php if (have_posts()) { ?>
+    <?php while (have_posts()) {
+        the_post(); ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-g"><?php the_content(); ?></div>
+                <div class="col-12 col-md-g"> <?php the_post_thumbnail("large"); ?></div>
+
+            </div>
+        </div>
+        
+    <?php } ?>
+<?php } ?>
+
+<?php get_footer() ?>
